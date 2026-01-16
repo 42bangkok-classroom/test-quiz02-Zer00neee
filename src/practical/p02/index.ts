b08ab8bf5a53bd365e9811810ac4963317d3ca16
68010126 <68010126@kmitl.ac.th>
2026-01-16 19:30:43 +0700

    add

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,32 +1,49 @@
 import axios from "axios";
 
 type Address = {
-  street: string ;
-  suite: string ;
-  city: string ;
-  zipcode: string ;
+  street: string | null;
+  suite: string | null;
+  city: string | null;
+  zipcode: string | null;
   geo: {
-    lat: string ;
-    lng: string ;
-  } ;
+    lat: string | null;
+    lng: string | null;
+  } | null;
 };
 
 type UserResult = {
   id: number;
-  name: string ;
-  phone: string ;
-  address: Address | null ;
+  name: string | null;
+  phone: string | null;
+  address: Address | null;
 };
 
 export async function addUser(newUserData: any): Promise<UserResult> {
   const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+  const users = res.data;
 
+  const lastId = users.length > 0 ? users[users.length - 1].id : 0;
 
-
-  return res.data = {
-    name: newUserData.name,
-    phone: newUserData.phone,
-    address: newUserData.address ?? null,
+  return {
+    id: lastId + 1,
+    name: newUserData.name ?? null,
+    phone: newUserData.phone ?? null,
+    address: newUserData.address
+      ? {
+          street: newUserData.address.street ?? null,
+          suite: newUserData.address.suite ?? null,
+          city: newUserData.address.city ?? null,
+          zipcode: newUserData.address.zipcode ?? null,
+          geo: newUserData.address.geo
+            ? {
+                lat: newUserData.address.geo.lat ?? null,
+                lng: newUserData.address.geo.lng ?? null,
+              }
+            : null,
+        }
+      : null,
   };
-  }
+}
+
+
 
